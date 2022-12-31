b955bbdfa6aac30240f2b5a56f83235b43329e4a
yeonnnee <yeonnnee@gmail.com>
2023-01-01 01:31:37 +0900

    header 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from "./components/Header";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <h1>Wh Am I</h1>
+        <div className="water-drop"></div>
+      </div>
+    </header>
+  );
+};
